8500debe319f61d8fb08b72661a1846d14ce5294
Bloyd-Wyrmii <735800@salemkeizer.org>
2026-05-22 15:46:34 -0700

    Implement enemy behavior and rendering
    Added enemy setup with movement and drawing functionality.

--- game.js
+++ game.js
@@ -7,12 +7,45 @@ gameCanvas.height = window.innerHeight;
 window.addEventListener("keydown", handleKeyAction);
 window.addEventListener("keyup", handleKeyAction);
 
-// IF YOU SEE THIS COMMENT GET OFF OF THE CODE THIS IS ONLY A DEMONSTRATION.
+// Your existing player setup
 playerPosition = {xPosition: gameCanvas.width / 2, yPosition: gameCanvas.height / 2};
 
-
-
 let keys = [];
+
+// --- NEW: Enemy Setup ---
+const enemies = [];
+
+class Enemy {
+  constructor(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+  }
+
+  update() {
+    // Note: Using your playerPosition variable names here
+    const dx = playerPosition.xPosition - this.x;
+    const dy = playerPosition.yPosition - this.y;
+    const angle = Math.atan2(dy, dx);
+
+    this.x += Math.cos(angle) * this.speed;
+    this.y += Math.sin(angle) * this.speed;
+  }
+
+  draw() {
+    gameCanvasContext.beginPath();
+    gameCanvasContext.fillStyle = "red";
+    gameCanvasContext.arc(this.x, this.y, 12, 0, Math.PI * 2);
+    gameCanvasContext.fill();
+    gameCanvasContext.closePath();
+  }
+}
+
+// Spawn a few enemies at the start
+for(let i = 0; i < 3; i++) {
+    enemies.push(new Enemy(Math.random() * gameCanvas.width, 0, 2));
+}
+// -----------------------
 
 function handleKeyAction(event) {
   if (event.type === "keydown") {
@@ -29,27 +62,11 @@ function handleKeyAction(event) {
   }
 }
 
-const arena = {
-  x: 50,          // Distance from canvas left edge
-  y: 50,          // Distance from canvas top edge
-  width: 700,     // Width of the playable zone
-  height: 500     // Height of the playable zone
-};
-
-
 function movePlayer() {
-  if (keys.includes(87)) {
-    playerPosition.yPosition -= 5;
-  }
-  if (keys.includes(65)) {
-    playerPosition.xPosition -= 5;
-  }
-  if (keys.includes(83)) {
-    playerPosition.yPosition += 5;
-  }
-  if (keys.includes(68)) {
-    playerPosition.xPosition += 5;
-  }
+  if (keys.includes(87)) playerPosition.yPosition -= 5;
+  if (keys.includes(65)) playerPosition.xPosition -= 5;
+  if (keys.includes(83)) playerPosition.yPosition += 5;
+  if (keys.includes(68)) playerPosition.xPosition += 5;
 }
 
 function drawPlayer() {
@@ -60,12 +77,21 @@ function drawPlayer() {
   gameCanvasContext.closePath();
 }
 
-
 function animate() {
   gameCanvasContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+  
   movePlayer();
   drawPlayer();
+
+  // --- NEW: Update and Draw Enemies ---
+  enemies.forEach(enemy => {
+    enemy.update();
+    enemy.draw();
+  });
+  // ------------------------------------
+
   requestAnimationFrame(animate);
 }
 
 animate();
+
